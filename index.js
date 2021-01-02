f203c6f638fb2b5e85f8b09618080e082b38b2b6
seagreenmedia <erinjenkins57@gmail.com>
2021-01-02 03:00:07 -0500

    everything but the event listener

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 class TemperatureConversion {
 
     calculate() {
-        let startingTemp = document.getElementById("startingTemp").value;
-        if (startingTemp === "fahrenheit") {
-            fahrenheitConversion();
-        } else if (startingTemp === "celsius") {
-            celsiusConversion();
-        } else if (startingTemp === "kelvin") {
-            kelvinConversion();
-        } else if (startingTemp === "rankine") {
-            rankineConversion();
-        }
+        {
+            document.getElementById("calButton").addEventListener("click", this.clickCalculate.bind(this));
+        };
+        clickCalculate() {
+            let startingTemp = document.getElementById("startingTemp").value;
+            if (startingTemp === "fahrenheit") {
+                fahrenheitConversion();
+            } else if (startingTemp === "celsius") {
+                celsiusConversion();
+            } else if (startingTemp === "kelvin") {
+                kelvinConversion();
+            } else if (startingTemp === "rankine") {
+                rankineConversion();
+            }
+        };
     }
 
     farenheitConversion(fahrenheit) {
-        let fahrenheit = parseInt(document.getElementById("conversionValue").value);
         document.getElementById("fahrenheit").innerHTML = parseFloat(fahrenheit).toFixed(2);
 
         let fahrenheitToCelsius = ((fahrenheit / 1.8) - 32);
@@ -28,7 +32,6 @@ class TemperatureConversion {
     }
 
     celsiusConversion(celsius) {
-        let celsius = parseInt(document.getElementById("conversionValue").value);
         document.getElementById("celsius").innerHTML = parseFloat(celsius).toFixed(2);
 
         let celsiusToFahrenheit = ((celsius * 1.8) + 32);
@@ -42,7 +45,6 @@ class TemperatureConversion {
     }
 
     kelvinConversion(kelvin) {
-        let kelvin = parseInt(document.getElementById("conversionValue").value);
         document.getElementById("kelvin").innerHTML = parseFloat(kelvin).toFixed(2);
 
         let kelvinToFahrenheit = ((kelvin * 1.8) - 459.67);
@@ -56,7 +58,6 @@ class TemperatureConversion {
     }
 
     rankineConversion(rankine) {
-        let rankine = parseInt(document.getElementById("conversionValue").value);
         document.getElementById("rankine").innerHTML = parseFloat(rankine).toFixed(2);
 
         let rankineToFahrenheit = rankine - 459.67;
@@ -68,7 +69,4 @@ class TemperatureConversion {
         let rankineToKelvin = rankine * (5 / 9);
         document.getElementById("kelvin").innerHTML = parseFloat(rankineToKelvin).toFixed(2);
     }
-
 }
-
-document.getElementById("calButton").addEventListener("click");
